a78c7ae0a43787301a10240643b38675007d76e7
Mahmood <mahmood5221lotfi@gmail.com>
2020-06-26 13:13:53 +0430

    add bootstrap to frontend

--- HospitalManagementSystem/HospitalManagementSystem/urls.py
+++ HospitalManagementSystem/HospitalManagementSystem/urls.py
@@ -25,6 +25,8 @@ urlpatterns = [
     url('', include('HospitalManagementApp.urls')),
     url('admin/', admin.site.urls),
     url(r'^accounts/', include('Accounts.urls')),
+    url(r'^accounts/login/$', views.login, name='login'),
+    url(r'^accounts/signup/$', views.signup, name='signup'),
     url(r'^account_activation_sent/$', views.account_activation_sent, name='account_activation_sent'),
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         views.activate, name='activate'),
