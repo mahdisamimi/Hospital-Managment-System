f64bfd5217146464168ff900a921389cb4e26084
Mahmood <mahmood5221lotfi@gmail.com>
2020-08-14 01:20:21 +0430

    add reserve managment

--- HospitalManagementSystem/Accounts/urls.py
+++ HospitalManagementSystem/Accounts/urls.py
@@ -4,8 +4,15 @@ from . import views
 
 app_name = 'accounts'
 urlpatterns = [
+    url(r'^add-manager/$', views.manager_signup , name='add-manager'),
     url(r'^usri/$', views.usri , name='usri'),
     url(r'^login/$', views.login, name='login'),
     url(r'^signup/$', views.signup, name='signup'),
-    url(r'^dashboard/$', views.dashboard, name='dashboard')
+    url(r'^rezervd/$', views.rezerv, name='rezerv'),
+    url(r'^dashboard/$', views.dashboard, name='dashboard'),
+    url(r'^rezerv/$', views.rezerv, name='rezerv'),
+    url(r'^rezerv_list/$', views.rezerv_list, name='rezerv_list'),
+    url(r'^delete/(?P<id>\d+)/$', views.delete_rezerv, name='delete'),
+    url(r'^update/(?P<id>\d+)/$', views.update_rezerv, name='update'),
+    url(r'^updating/$', views.updating, name='updating'),
 ]
--- HospitalManagementSystem/HospitalManagementApp/forms.py
+++ HospitalManagementSystem/HospitalManagementApp/forms.py
@@ -1,8 +1,20 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
-from HospitalManagementApp.models import doctor
-from.models import base_user
+from django.contrib.auth.models import User , Group
+from HospitalManagementApp.models import doctor,clerk,free_time
+from.models import base_user ,reserve
+from django.forms import ModelForm
+import datetime
+class ReservesForm(ModelForm):
+    class Meta:
+        model = reserve
+        fields = ['doctor_id', 'reserve_time', 'natural_code', 'clerk_id', ]
+
+    doctor_id = forms.ModelChoiceField(queryset=doctor.objects.all())
+    reserve_time = forms.ModelChoiceField(queryset=free_time.objects.all())
+    natural_code = forms.IntegerField()
+    clerk_id = forms.ModelChoiceField(queryset=clerk.objects.all())
+    # !!ADVANCED!! hospital = models.ForeignKey(hospital, on_delete=models.CASCADE)
 
 
 
@@ -34,5 +46,24 @@ class ClerkSignUpForm(UserCreationForm):
         fields = ('username', 'first_name', 'last_name', 'natural_code', 'phone', 'email', )
     def __init__(self, *args, **kargs):
         super(ClerkSignUpForm, self).__init__(*args, **kargs)
+        self.fields.pop('password1')
+        self.fields.pop('password2')
+
+class ManagerSignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=255, required=True)
+    last_name = forms.CharField(max_length=255, required=True)
+    natural_code = forms.IntegerField(required=True)
+    phone = forms.IntegerField(required=True)
+    email = forms.CharField(max_length=40, required=True,
+                            help_text="Enter valid Email Address. You will be asked for Verification.")
+    hospital_name = forms.CharField(max_length=255, required=True)
+    hospital_id = forms.IntegerField(required=True)
+
+    class Meta:
+        model = base_user
+        fields = ('username', 'first_name', 'last_name', 'natural_code', 'phone', 'email', 'hospital_name', 'hospital_id', )
+
+    def __init__(self, *args, **kargs):
+        super(ManagerSignUpForm, self).__init__(*args, **kargs)
         self.fields.pop('password1')
         self.fields.pop('password2')
